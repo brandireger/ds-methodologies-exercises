b53ead7747c742fc4a3911a3a764d84d60d5fc36
brandireger <brandi.reger@gmail.com>
2020-05-13 16:01:53 -0500

    added explore exercises

--- time_series/prepare.py
+++ time_series/prepare.py
@@ -4,6 +4,12 @@ import os
 
 from datetime import timedelta, datetime
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.rc('figure', figsize=(11, 9))
+plt.rc('font', size=13)
+
 import acquire
 
 #-----------#
@@ -11,7 +17,7 @@ import acquire
 #-----------#
 
 def set_datetime_index(df, col):
-    df[col] = pd.to_datetime(df[col])
+    df[col] = pd.to_datetime(df[col], format='%a, %d %b %Y %H:%M:%S %Z')
     df = df.sort_values(col).set_index(col)
     return df
 
@@ -40,22 +46,17 @@ def make_sales_diff_col(df):
 #--------------#
 
 def prep_store_data():
-    # check if csv exists, if not create raw df to be modified
-    if os.path.exists('prepped_store_data.csv'):
-        df = pd.read_csv('prepped_store_data.csv', index_col=0)
-        return df
-    else:
-        df = get_store_data()
-        # prep df per instructions: set sale_date as datetime column, make it the index, make new month col based on the sale_date index, make new day col based on sale_date index
-        df = set_datetime_index(df, 'sale_date')    
-        df = make_month_col(df)
-        df = make_day_col(df)
-        # create a new column derived from sale_amount (total items) and item_price
-        df = make_sales_total_col(df)
-        # create a new column that is the result of the current sales - the previous days sales
-        df = make_sales_diff_col(df)
-        # write modified df to new csv file
-        df.to_csv('prepped_store_data.csv')
+    df = acquire.get_store_data()
+    # prep df per instructions: set sale_date as datetime column, make it the index, make new month col based on the sale_date index, make new day col based on sale_date index
+    df = set_datetime_index(df, 'sale_date')    
+    df = make_month_col(df)
+    df = make_day_col(df)
+    # create a new column derived from sale_amount (total items) and item_price
+    df = make_sales_total_col(df)
+    # create a new column that is the result of the current sales - the previous days sales
+    df = make_sales_diff_col(df)
+    # write modified df to new csv file
+    df.to_csv('prepped_store_data.csv')
     return df
 
 #---------------------#
@@ -63,16 +64,40 @@ def prep_store_data():
 #---------------------#
 
 def prep_ops_data():
-    # check if csv exists, if not create raw df to be modified
-    if os.path.exists('prepped_ops_data.csv'):
-        df = pd.read_csv('prepped_ops_data.csv', index_col=0)
-        return df
-    else:
-        df = acquire.read_german()
-        # prep df per instructions: set Date as datetime column, make it the index, make new month col based on the Date index, make new year col based on Date index
-        df = set_datetime_index(df, 'Date')    
-        df = make_month_col(df)
-        df = make_year_col(df)
-        # write modified df to new csv file
-        df.to_csv('prepped_ops_data.csv')
+    df = acquire.read_german()
+    # prep df per instructions: set Date as datetime column, make it the index, make new month col based on the Date index, make new year col based on Date index
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date').set_index('Date')   
+    df = make_month_col(df)
+    df = make_year_col(df)
+    # write modified df to new csv file
+    df.to_csv('prepped_ops_data.csv')
     return df
+
+#------------------#
+#  Visualizations  #
+#------------------#
+
+def hist_plot(df, col, bins):
+    """
+    function takes in a DataFrame, 
+    a string for column name or list, and 
+    integer for number of bins and
+    displays a histogram of the column
+    """
+    plt.hist(df[col], bins=bins, color='thistle')
+    plt.title('Distribution of ' + col)
+    plt.xlabel('')
+    plt.ylabel('Count')
+    plt.show()
+    
+def numeric_hists(df, bins=20):
+    """
+    Function to take in a DataFrame, bins default 20,
+    select only numeric dtypes, and
+    display histograms for each numeric column
+    """
+    num_df = df.select_dtypes(include=np.number)
+    num_df.hist(bins=bins, color='thistle')
+    plt.suptitle('Numeric Column Distributions')
+    plt.show()
